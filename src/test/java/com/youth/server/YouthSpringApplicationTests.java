b93d22512dac2964cfbf2dbd4015ef992bdfb6ab
seo0jjjjj <seo0jjjjj@mail.com>
2024-07-19 17:30:33 +0900

    Initial commit

---
+++ src/test/java/com/youth/server/YouthSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youth.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouthSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
