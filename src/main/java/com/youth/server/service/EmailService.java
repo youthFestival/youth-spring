561171f7095df7b84d1243e95d26b0ba3f67df04
OrangeBoy98 <wogks119@gmail.com>
2024-07-29 12:28:43 +0900

    feat: 인증메일 발신자 이름 변경 기능 추가 SimpleMailMessage 에서 MimeMessage 으로 변경

--- src/main/java/com/youth/server/service/EmailService.java
+++ src/main/java/com/youth/server/service/EmailService.java
@@ -1,12 +1,17 @@
 package com.youth.server.service;
 
 import com.youth.server.exception.BusinessLogicException;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import java.io.UnsupportedEncodingException;
 
 @Slf4j
 @Service
@@ -15,8 +20,27 @@ import org.springframework.stereotype.Service;
 public class EmailService {
     private final JavaMailSender javaMailSender;
 
+//    public void sendEmail(String toEmail, String title, String text) {
+//        SimpleMailMessage emailForm = createEmailForm(toEmail, title, text);
+//        try {
+//            javaMailSender.send(emailForm);
+//        } catch (RuntimeException e) {
+//            log.debug("MailService.sendEmail exception occur toEmail: {}, " +
+//                    "title: {}, text: {}", toEmail, title, text);
+//            throw new BusinessLogicException(BusinessLogicException.ExceptionCode.UNABLE_TO_SEND_EMAIL);
+//        }
+//    }
+//
+//    private SimpleMailMessage createEmailForm(String toEmail, String title, String text) {
+//        SimpleMailMessage message = new SimpleMailMessage();
+//        message.setTo(toEmail);
+//        message.setSubject(title);
+//        message.setText(text);
+//
+//        return message;
+//    }
     public void sendEmail(String toEmail, String title, String text) {
-        SimpleMailMessage emailForm = createEmailForm(toEmail, title, text);
+        MimeMessage emailForm = createEmailForm(toEmail, title, text);
         try {
             javaMailSender.send(emailForm);
         } catch (RuntimeException e) {
@@ -26,12 +50,18 @@ public class EmailService {
         }
     }
 
-    private SimpleMailMessage createEmailForm(String toEmail, String title, String text) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(toEmail);
-        message.setSubject(title);
-        message.setText(text);
+        private MimeMessage createEmailForm(String toEmail, String title, String text) {
+            MimeMessage message = javaMailSender.createMimeMessage();
+            try {
+                MimeMessageHelper helper = new MimeMessageHelper(message, true);
+                helper.setTo(toEmail);
+                helper.setSubject(title);
+                helper.setText(text);
+                helper.setFrom(new InternetAddress("wogks119@gmail.com", "YouthSupport")); // 발신자 이름을 포함하여 설정
+            } catch (MessagingException | UnsupportedEncodingException e) {
+                throw new RuntimeException("Failed to create email form", e);
+            }
 
-        return message;
-    }
+            return message;
+        }
 }
